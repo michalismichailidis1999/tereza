7c2ef8b83ba8da9cb7a24c226fe047e0fec22650
Michalis Michailidis <mixalismixailidis857@gmail.com>
2020-12-29 00:27:34 +0200

    Tag Manager Added

--- includes/header.php
+++ includes/header.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-MRR8J9P');</script>
+    <!-- End Google Tag Manager -->
+
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Η Τερέζα Μιχαηλίδου είναι μέλος του Δικηγορικού Συλλόγου Βέροιας. 
@@ -32,6 +35,8 @@
     <title>Τερέζα Σ. Μιχαηλίδου - Δικηγόρος</title>
 </head>
 <body>
+    <!-- Google Tag Manager (noscript) -->
     <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MRR8J9P"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <?php require_once("config/constants.php") ?>
